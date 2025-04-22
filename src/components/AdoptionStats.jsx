785a00ae198e45316815c86d1b57e722bdb368f4
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-04-22 15:59:51 +0530

    half stage

--- src/components/AdoptionStats.jsx
+++ src/components/AdoptionStats.jsx
@@ -7,12 +7,18 @@ const AdoptionStats = () => {
   let dogsListed = 800;
 
   return (
-    <div className="hero-section flex flex-col items-start justify-center min-h-screen text-white p-6">
-      <div className="w-full max-w-4xl text-center">
-        <h2 className="text-5xl font-semibold mb-8">
+    <div className="hero-section custom flex flex-col items-start sm:justify-center justify-start  min-h-screen text-white p-6">
+      <div className="w-full max-w-4xl  text-center">
+        <h1
+          className="text-4xl md:text-5xl font-bold md:font-semibold 
+              text-center md:text-left 
+              px-4 md:px-0 
+              leading-tight md:leading-normal
+              mb-6 mt-6 md:mb-8
+              max-w-2xl md:max-w-4xl mx-auto md:mx-0">
           Helping you adopt indies around you
-        </h2>
-        <div className="flex justify-center space-x-16 mb-12">
+        </h1>
+        <div className="hidden md:flex justify-center space-x-16 mb-12">
           <div>
             <p className="text-4xl font-bold">{dogsAdopted}</p>
             <p className="text-3xl">dogs adopted</p>
@@ -22,15 +28,16 @@ const AdoptionStats = () => {
             <p className="text-3xl">dogs listed</p>
           </div>
         </div>
+
         <div className="flex flex-col space-y-6 w-full max-w-md mx-auto">
           <Link
             to="/map"
-            className="border border-white text-white text-2xl py-4 px-8 rounded-lg transition-colors duration-200">
+            className="border custom-button border-white text-2xl py-4 px-8 rounded-lg transition-colors duration-200">
             Find Dogs to Adopt
           </Link>
           <Link
             to="/list-dog"
-            className="border border-white text-white text-2xl py-4 px-8 rounded-lg transition-colors duration-200">
+            className="border custom-button border-white text-2xl py-4 px-8 rounded-lg transition-colors duration-200">
             List a Dog
           </Link>
         </div>
