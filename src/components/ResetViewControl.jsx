11ffb746af5a9090db523206ccec53725d5e2b95
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-04-22 20:29:07 +0530

    half stage

--- src/components/ResetViewControl.jsx
+++ src/components/ResetViewControl.jsx
@@ -34,7 +34,7 @@ function ResetViewControl({ initialPosition, initialZoom }) {
       <div className="leaflet-control">
         <button
           onClick={resetView}
-          className=" sm:mb-15 mb-25 mr-3  bg-white rounded-full   "
+          className=" sm:mb-15 mb-35 mr-3  bg-white rounded-full   "
           title="Reset to original view">
           <img
             src="./images/locate.svg"
